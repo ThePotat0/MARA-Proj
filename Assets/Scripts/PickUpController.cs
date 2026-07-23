369c41e3b1b9ff754a936ad243bc67fbd7917ea7
ThePotat0 <markbrusman@gmail.com>
2026-07-23 19:35:26 +0300

    Механика подбора предметов

--- Assets/Scripts/PickUpController.cs
+++ Assets/Scripts/PickUpController.cs
@@ -6,33 +6,90 @@ public class PickUpController : MonoBehaviour
     private bool _onClueTrigger = false;
     private Collider _clueObject;
     [SerializeField] private GameObject _clue;
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private float _pickUpDistance = 3f;
+    [SerializeField] private string _itemName = "Item";
+
+    private void Awake()
+    {
+        if (_cameraTransform == null && Camera.main != null)
+        {
+            _cameraTransform = Camera.main.transform;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("ClueArea"))
+        {
+            return;
+        }
+
         _onClueTrigger = true;
-        Debug.Log(_onClueTrigger);
         _clueObject = other;
-        _clue.SetActive(true);
-        Debug.Log(_clueObject.gameObject.name + " entered clue area!");
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other != _clueObject)
+        {
+            return;
+        }
+
         _clueObject = null;
         _onClueTrigger = false;
-        Debug.Log(_onClueTrigger);
-        _clue.SetActive(false);
-        Debug.Log(other.gameObject.name + " exited clue area!");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(_cameraTransform.position, _cameraTransform.forward, out RaycastHit hit, _pickUpDistance, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore) && _onClueTrigger && _clue != null)
+        {
+            _clue.SetActive(true);
+        }
+        else
+        {
+            _clue.SetActive(false);
+        }
     }
 
     private void Update()
     {
-        if (_clueObject != null && _clueObject.tag == "ClueArea")
+        if (Keyboard.current == null)
         {
-            if (Keyboard.current.eKey.wasPressedThisFrame && _onClueTrigger)
+            return;
+        }
+
+        if (_clueObject != null && _clueObject.CompareTag("ClueArea"))
+        {
+            if (Keyboard.current.eKey.wasPressedThisFrame && _onClueTrigger && IsLookingAtItem(out GameObject item))
             {
-                Destroy(_clueObject.gameObject);
-                Debug.Log(_clueObject.gameObject.name + " picked up!");
+                Destroy(item);
+                Debug.Log(item.name + " picked up!");
             }
         }
+    }
+
+    private bool IsLookingAtItem(out GameObject item)
+    {
+        item = null;
+
+        if (_cameraTransform == null)
+        {
+            return false;
+        }
+
+        if (!Physics.Raycast(_cameraTransform.position, _cameraTransform.forward, out RaycastHit hit, _pickUpDistance, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
+        {
+            return false;
+        }
+
+        GameObject hitObject = hit.collider.gameObject;
+        if (hitObject.name != _itemName)
+        {
+            return false;
+        }
+
+        item = hitObject;
+        return true;
     }
 }
